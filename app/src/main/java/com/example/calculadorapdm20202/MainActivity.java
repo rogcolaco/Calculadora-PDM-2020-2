e23194ac433509989cdad628af55929af51db20a
Rogerio Colaço <rcs@ufscar.br>
2020-10-20 21:46:44 -0300

    - botões iniciais (0,1,2) - layout inicial

--- app/src/main/java/com/example/calculadorapdm20202/MainActivity.java
+++ app/src/main/java/com/example/calculadorapdm20202/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.calculadorapdm20202;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    private TextView visorTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        visorTv=findViewById(R.id.visorTv);
+    }
+
+    public void onClick(View view){
+        switch ((view.getId())) {
+            case R.id.zeroBtn:
+                visorTv.setText(getString(R.string.zero));
+                Log.v(getString(R.string.app_name),getString(R.string.zero));
+                break;
+            case R.id.umBtn:
+                visorTv.setText(getString(R.string.um));
+                break;
+            case R.id.doisBtn:
+                visorTv.setText(getString(R.string.dois));
+                break;
+            case R.id.limparBtn:
+                visorTv.setText(" ");
+                break;
+        }
+
     }
 }
