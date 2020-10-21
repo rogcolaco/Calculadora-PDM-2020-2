06ab619b3c1bd84a646f987010d34fc136cf2a4d
Rogerio Colaço <rcs@ufscar.br>
2020-10-20 22:00:54 -0300

    - Inclusão de todos os botões da calculadora; - alteração do strings.xml; - alteração do posicionamento do número zero e a distribuição com o botão limpar;

--- app/src/main/java/com/example/calculadorapdm20202/MainActivity.java
+++ app/src/main/java/com/example/calculadorapdm20202/MainActivity.java
@@ -7,8 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity{
     private TextView visorTv;
+    private String value = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +21,31 @@ public class MainActivity extends AppCompatActivity{
         visorTv=findViewById(R.id.visorTv);
     }
 
+    public void fillVisor(String s){
+        visorTv.setText(s);
+    }
+
     public void onClick(View view){
         switch ((view.getId())) {
             case R.id.zeroBtn:
-                visorTv.setText(getString(R.string.zero));
-                Log.v(getString(R.string.app_name),getString(R.string.zero));
+                value = value+(getString(R.string.zero));
+                fillVisor(value);
+                //visorTv.setText(getString(R.string.zero));
+                //Log.v(getString(R.string.app_name),getString(R.string.zero));
                 break;
             case R.id.umBtn:
-                visorTv.setText(getString(R.string.um));
+                value = value+(getString(R.string.um));
+                fillVisor(value);
+                //visorTv.setText(getString(R.string.um));
                 break;
             case R.id.doisBtn:
-                visorTv.setText(getString(R.string.dois));
+                value = value+(getString(R.string.dois));
+                fillVisor(value);
+                //visorTv.setText(getString(R.string.dois));
                 break;
             case R.id.limparBtn:
-                visorTv.setText(" ");
+                value = "";
+                fillVisor(value);
                 break;
         }
 
